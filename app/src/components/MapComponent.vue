ea97ad26183c61d8ee42860c2cee892f53633f3d
wShatner <eetu.leivo@edu.savonia.fi>
2017-11-24 21:13:00 +0200

    Changed map

--- app/src/components/MapComponent.vue
+++ app/src/components/MapComponent.vue
@@ -11,20 +11,20 @@
         },
 
         mounted() {
-            this.buildMap();
+            this.initMap();
         },
 
         methods: {
-            buildMap() {
-                mapboxgl.accessToken = this.accessToken;
-
-                var map = new mapboxgl.Map({
-                    container: 'map', // container id
-                    style: 'mapbox://styles/mapbox/streets-v10', // stylesheet location
-                    center: [-74.50, 40], // starting position [lng, lat]
-                    zoom: 9 // starting zoom
+            initMap() {
+                var uluru = {lat: -25.363, lng: 131.044};
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 4,
+                    center: uluru
                 });
-                map.resize();
+                var marker = new google.maps.Marker({
+                    position: uluru,
+                    map: map
+                });
             }
         }
     }
@@ -32,5 +32,6 @@
 
 <style>
     body { margin:0; padding:0; }
-    #map { position:absolute; top:0; bottom:0; width:100%; height: 100%; }
+    #map {  height: 100%;
+        width: 100%; }
 </style>
