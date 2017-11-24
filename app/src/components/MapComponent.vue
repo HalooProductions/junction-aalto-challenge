0fce3aaa9df70d1bbe60a85ad2ebaa56b21b0e2b
wShatner <eetu.leivo@edu.savonia.fi>
2017-11-25 00:59:40 +0200

    Map is able to jump to location...to be continued

--- app/src/components/MapComponent.vue
+++ app/src/components/MapComponent.vue
@@ -6,24 +6,79 @@
     export default {
         data() {
             return {
-                accessToken: "pk.eyJ1Ijoic2hhdG5lciIsImEiOiJjamFkcWo1cGoxbng1MzdxbWJ1M2p1MDZzIn0.kXyKDLhGmdrsK4sHkY6Z5A"
+                accessToken: "pk.eyJ1Ijoic2hhdG5lciIsImEiOiJjamFkcWo1cGoxbng1MzdxbWJ1M2p1MDZzIn0.kXyKDLhGmdrsK4sHkY6Z5A",
+                map: null
             }
         },
 
         mounted() {
             this.buildMap();
+            setTimeout(() => {
+                this.jumpToLocation([24.855831, 60.192059])
+            }, 5000);
         },
 
         methods: {
             buildMap() {
+                let data = {
+                    coordinates: [24.945831, 60.192059],
+                    testData: {
+                        type: 'FeatureCollection',
+                        features: [{
+                            type: 'Feature',
+                            geometry: {
+                            type: 'Point',
+                            coordinates: [24.945831, 60.192059]
+                            },
+                            properties: {
+                            title: 'Mapbox',
+                            description: 'Washington, D.C.'
+                            }
+                        },
+                        {
+                            type: 'Feature',
+                            geometry: {
+                            type: 'Point',
+                            coordinates: [24.945, 60.192060]
+                            },
+                            properties: {
+                            title: 'Mapbox',
+                            description: 'San Francisco, California'
+                            }
+                        }]
+                    }
+                };
+
                 mapboxgl.accessToken = this.accessToken;
 
-                var map = new mapboxgl.Map({
+                this.map = new mapboxgl.Map({
                     container: 'map', // container id
                     style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
-                    center: [24.945831, 60.192059], // starting position [lng, lat]
+                    center: data.coordinates, // starting position [lng, lat]
                     zoom: 14 // starting zoom
                 });
+
+                this.buildMarkers(data.testData)
+            },
+
+            buildMarkers(testData) {
+
+                // add markers to map
+                testData.features.forEach((marker) => {
+
+                    // create a HTML element for each feature
+                    var el = document.createElement('div');
+                    el.className = 'marker';
+
+                    // make a marker for each feature and add to the map
+                    new mapboxgl.Marker(el)
+                    .setLngLat(marker.geometry.coordinates)
+                    .addTo(this.map);
+                });
+            },
+
+            jumpToLocation(location) {
+                this.map.flyTo({center: location})
             }
         }
     }
@@ -32,4 +87,12 @@
 <style>
     body { margin:0; padding:0; }
     #map { position:absolute; top:0; bottom:0; width:100%; height: 100%; }
+    .marker {
+        background-image: url('../assets/mapbox-icon.png');
+        background-size: cover;
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        cursor: pointer;
+    }
 </style>
