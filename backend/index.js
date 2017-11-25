ec7907ef4aa6a0f7d19aefa4f0a04c0e5bfb4077
rikuw <riku.wikman@gmail.com>
2017-11-25 12:32:34 +0200

    Serving index page and static files added

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,38 @@
 import axios from 'axios';
+import express from 'express';
+import path from 'path';
 
-const http = require('http');
-const port = 3000;
 const baseApiUrl = 'https://llb.cloud.tyk.io/llb-bus-api/';
 const busDataUrl = 'GetData?busId=';
 const authStr = 'Bearer 5a07a2f986f30e00015b3cb1b4768fc0e06940ee8c440c550a42fec7';
 
-const requestHandler = (request, response) => {
-    console.log(request.url);
-    /*axios.get('http://llb.vtt.fi/LLBDataAPI/GetData?busId=1612').then(result => {
+let app = express();
+
+app.get('/buslocation/:busid', (req, res) => {
+    getBusDataLocation(1612).then(result => {
         console.log(result);
-    });*/
+        let stringData = JSON.stringify(result);
+        res.send(stringData);
+    }).catch(error => {
+        console.log(error);
+        res.send('error getting bus location');
+    });
+});
+
+app.use('/static', express.static('static'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('index.html'));
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address
+    var port = server.address().port
+    
+    console.log("Example app listening at http://%s:%s", host, port)
+ });
+
+/*const requestHandler = (request, response) => {
     getBusDataLocation(1612).then(result => {
         console.log("resulted");
         console.log(result);
@@ -19,7 +41,7 @@ const requestHandler = (request, response) => {
     }).catch(error => {
         console.log(error);
     });
-}
+}*/
 
 /*axios.get('http://llb.vtt.fi/LLBDataAPI/GetData?busId=1612', { 'headers': { 'Authorization': authStr } }).then(result => {
     console.log(result);
@@ -27,7 +49,7 @@ const requestHandler = (request, response) => {
     console.log(error);
 });*/
 
-const server = http.createServer(requestHandler);
+/*const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
     if (err) {
@@ -35,7 +57,7 @@ server.listen(port, (err) => {
     }
 
     console.log(`server is listening on ${port}`);
-});
+});*/
 
 const getBusDataLocation = busId => {
     console.log('getBusData');
