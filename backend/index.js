eb82a284069000a11f09503fd94ffcd013d24bf8
moona.partanen <moona.partanen@edu.savonia.fi>
2017-11-25 16:47:06 +0200

    Resonse tulee pysäkeistä

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ const baseApiUrl = 'https://llb.cloud.tyk.io/llb-bus-api/';
 const busDataUrl = 'GetData?busId=';
 const authStr = 'Bearer 5a07a2f986f30e00015b3cb1b4768fc0e06940ee8c440c550a42fec7';
 
+const stopUrl = "http://api.digitransit.fi/routing/v1/routers/hsl/index/graphql";
+
 let app = express();
 
 app.get('/buslocation/:busid', (req, res) => {
@@ -25,6 +27,36 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve('index.html'));
 });
 
+app.get('/nextstop/:name', (req, res) => {
+
+    let configGraphQL = {
+        url: 'http://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
+        method: 'post', 
+        headers: { 'Content-Type': 'application/graphql' },
+        data: `{
+            pattern(id:"HSL:1023:1:01") {
+              name
+              stops{
+                name  
+              }
+            }
+          }`
+    };
+
+    axios(configGraphQL).then(response => {
+        console.log('graphql response:', response.data); 
+               let stopData = JSON.stringify(response.data);
+        res.send(stopData);
+    }).catch(err => {
+        console.log('graphql error:', err);
+    });
+
+
+
+    //console.log(result);
+    //res.send(response.data);
+});
+
 var server = app.listen(3000, function () {
     var host = server.address().address
     var port = server.address().port
@@ -40,7 +72,7 @@ const getBusDataLocation = busId => {
             reject('no busid provided');
         }
     
-        axios.get(baseApiUrl + busDataUrl + busId, { 'headers': { 'Authorization': authStr } }).then(response => {
+        axios.get(baseApiUrl + busDataUrl + busId, { 'headers':  { 'Authorization': authStr } }).then(response => {
             console.log("resolving");
             resolve({lat: response.data.lat, lon: response.data.lon});
         })
@@ -50,3 +82,4 @@ const getBusDataLocation = busId => {
         });
     });
 }
+
