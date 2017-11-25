1c1e166f8f2ca8bd692e9d846224b47e2b995b5f
rikuw <riku.wikman@gmail.com>
2017-11-25 15:39:27 +0200

    Weather API added

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import path from 'path';
 const baseApiUrl = 'https://llb.cloud.tyk.io/llb-bus-api/';
 const busDataUrl = 'GetData?busId=';
 const authStr = 'Bearer 5a07a2f986f30e00015b3cb1b4768fc0e06940ee8c440c550a42fec7';
+const weatherApiUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=Helsinki,FI&appid=453382cec788339c262b0ffbf0ec4ff0&units=metric';
 
 let app = express();
 
@@ -12,10 +13,24 @@ app.get('/buslocation/:busid', (req, res) => {
     getBusDataLocation(req.params.busid).then(result => {
         console.log(result);
         let stringData = JSON.stringify(result);
+
+        res.setHeader('Content-Type', 'application/json');
         res.send(stringData);
     }).catch(error => {
         console.log(error);
         res.send('error getting bus location');
+    });
+});
+
+app.get('/weather', (req, res) => {
+    getWeatherInfo().then(result => {
+        let data = JSON.stringify(result);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data);
+    })
+    .catch(error => {
+        console.log('rejected');
+        res.send(error);
     });
 });
 
@@ -46,6 +61,28 @@ const getBusDataLocation = busId => {
         })
         .catch(error => {
             console.log(error);
+            reject(error);
+        });
+    });
+}
+
+const getWeatherInfo = () => {
+    return new Promise((resolve, reject) => {
+        axios.get(weatherApiUrl).then(response => {
+            let list = response.data.list;
+            resolve({
+                currentWeather: {
+                    temp: list[0].main.temp,
+                    weatherType: list[0].weather[0].main
+                },
+                nextWeather: {
+                    temp: list[1].main.temp,
+                    weatherType: list[1].weather[0].main
+                }
+            });
+        })
+        .catch(error => {
+            console.log('rejecting');
             reject(error);
         });
     });
